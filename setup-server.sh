30d2e8cbdbef6b9a1b91075e8e01adf9de2fa9b6
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-10-22 13:08:12 +0200

    Update

--- setup-server.sh
+++ setup-server.sh
@@ -5,5 +5,3 @@ systemctl disable --now docker.socket rpm-ostree-countme.timer
 systemctl enable --now podman-auto-update.timer
 systemctl enable --global podman-auto-update.timer
 systemd-tmpfiles --create
-rm -rf /etc/containers/systemd/users/"$(id -u nginx)"
-cp -R /etc/containers/users/nginx /etc/containers/systemd/users/"$(id -u nginx)"
--- setup-vps.sh
+++ setup-vps.sh
@@ -3,7 +3,6 @@ set -e
 cp -R vps/* /
 ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sh setup-server.sh
-rm -rf /etc/containers/systemd/users/"$(id -u coturn)"
-rm -rf /etc/containers/systemd/users/"$(id -u podman-mail)"
-cp -R /etc/containers/users/coturn /etc/containers/systemd/users/"$(id -u coturn)"
-cp -R /etc/containers//users/podman-mail /etc/containers/systemd/users/"$(id -u podman-mail)"
+cp -R containers-vps/coturn /etc/containers/systemd/users/"$(id -u coturn)"
+cp -R containers-vps/nginx /etc/containers/systemd/users/"$(id -u nginx)"
+cp -R containers-vps/podman-mail /etc/containers/systemd/users/"$(id -u podman-mail)"
