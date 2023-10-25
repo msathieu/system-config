ea26436c389df0a755fca77837ae400d3d828c6c
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-10-25 16:26:34 +0200

    Update

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -4,5 +4,6 @@ cp -R desktop/* laptop/* /
 sh setup-desktop.sh
 systemctl enable --now sshd
 systemctl mask --global grub-boot-success.timer
-flatpak remote-add --if-not-exists flatpak /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
+flatpak remote-modify --subset=floss flathub
 flatpak install com.github.micahflee.torbrowser-launcher com.github.tchx84.Flatseal com.vscodium.codium im.riot.Riot io.mpv.Mpv org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox
