86f4d6955a4ea4c03f93fba2c3f8de47b1297020
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-02 15:28:52 +0100

    Update

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -6,4 +6,4 @@ systemctl enable --now sshd
 systemctl mask --global grub-boot-success.timer
 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
 flatpak remote-modify --subset=floss flathub
-flatpak install com.github.micahflee.torbrowser-launcher com.github.tchx84.Flatseal com.vscodium.codium im.riot.Riot io.mpv.Mpv org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox
+flatpak install com.github.micahflee.torbrowser-launcher com.github.tchx84.Flatseal com.vscodium.codium com.yubico.yubioath im.riot.Riot io.mpv.Mpv org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox
--- setup-qubes-fedora.sh
+++ setup-qubes-fedora.sh
@@ -2,7 +2,7 @@
 set -e
 cp -R desktop/* qubes-fedora/* /
 dnf config-manager --set-enabled qubes-vm-r4.2-security-testing qubes-vm-r4.2-current-testing rpmfusion-free rpmfusion-free-updates
-dnf install --allowerasing android-tools bash-completion bind-utils btrfs-progs butane evince fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller flac flatseal fuse-sshfs gjs gnome-calculator gnome-keyring gnome-terminal gnome-terminal-nautilus gnome-text-editor google-noto-sans-symbols2-fonts htop imv libreoffice mozilla-ublock-origin mpv nautilus netcat pipewire-pulseaudio pipewire-qubes python3-mutagen rclone sequoia-sq ShellCheck sox toolbox vim-trailing-whitespace wireguard-tools whois xdg-desktop-portal-gtk yt-dlp
+dnf install --allowerasing bash-completion bind-utils btrfs-progs butane evince fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller flatseal fuse-sshfs gjs gnome-calculator gnome-keyring gnome-terminal gnome-terminal-nautilus gnome-text-editor google-noto-sans-symbols2-fonts htop imv libreoffice mozilla-ublock-origin mpv nautilus netcat pipewire-pulseaudio pipewire-qubes qubes-ctap sequoia-sq ShellCheck toolbox vim-trailing-whitespace wireguard-tools whois xdg-desktop-portal-gtk yt-dlp
 dnf remove thunderbird @gnome
 dnf group install --assumeyes fonts
 sh setup-desktop.sh
