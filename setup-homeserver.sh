ac1a598013b3bddf59fdeac3539b7be883d4a85c
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-04 19:34:05 +0100

    Update

--- setup-homeserver.sh
+++ setup-homeserver.sh
@@ -6,7 +6,5 @@ rpm-ostree install --idempotent systemd-networkd wireless-regdb
 systemctl enable --now nftables systemd-networkd
 systemctl disable --now NetworkManager
 cp -R containers-home/gitea /etc/containers/systemd/users/"$(id -u gitea)"
-cp -R containers-home/monero /etc/containers/systemd/users/"$(id -u monero)"
 cp -R containers-home/nginx /etc/containers/systemd/users/"$(id -u nginx)"
 cp -R containers-home/synapse /etc/containers/systemd/users/"$(id -u synapse)"
-cp -R containers-home/vaultwarden /etc/containers/systemd/users/"$(id -u vaultwarden)"
--- setup-vps.sh
+++ setup-vps.sh
@@ -3,6 +3,3 @@ set -e
 cp -R vps/* /
 ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sh setup-server.sh
-cp -R containers-vps/coturn /etc/containers/systemd/users/"$(id -u coturn)"
-cp -R containers-vps/nginx /etc/containers/systemd/users/"$(id -u nginx)"
-cp -R containers-vps/podman-mail /etc/containers/systemd/users/"$(id -u podman-mail)"
