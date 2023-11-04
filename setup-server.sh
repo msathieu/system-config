8af9ac3e6b1d1fd97223b54a916e308ff7ddf3ea
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-04 08:11:27 +0100

    Update

--- setup-server.sh
+++ setup-server.sh
@@ -5,4 +5,5 @@ systemctl disable --now docker.socket rpm-ostree-countme.timer
 systemctl enable --now podman-auto-update.timer
 systemctl enable --global podman-auto-update.timer
 systemd-tmpfiles --create
+useradd -M containers || true
 rm -rf /etc/containers/systemd/users/*
