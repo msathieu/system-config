b8c84577f82ea6adce6f73f463b2ac140a29ce78
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-05 10:08:49 +0100

    Update

--- setup-homeserver.sh
+++ setup-homeserver.sh
@@ -5,6 +5,3 @@ sh setup-server.sh
 rpm-ostree install --idempotent systemd-networkd wireless-regdb
 systemctl enable --now nftables systemd-networkd
 systemctl disable --now NetworkManager
-cp -R containers-home/gitea /etc/containers/systemd/users/"$(id -u gitea)"
-cp -R containers-home/nginx /etc/containers/systemd/users/"$(id -u nginx)"
-cp -R containers-home/synapse /etc/containers/systemd/users/"$(id -u synapse)"
--- setup-server.sh
+++ setup-server.sh
@@ -3,7 +3,4 @@ set -e
 cp -R server/* /
 systemctl disable --now docker.socket rpm-ostree-countme.timer
 systemctl enable --now podman-auto-update.timer
-systemctl enable --global podman-auto-update.timer
-systemd-tmpfiles --create
 useradd -M containers || true
-rm -rf /etc/containers/systemd/users/*
