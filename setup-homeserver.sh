38b86ab6157da4926898d2de23268bdea9cfd596
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-18 15:15:59 +0100

    Add tmpfiles.d for act-runner

--- setup-homeserver.sh
+++ setup-homeserver.sh
@@ -4,4 +4,6 @@ cp -R homeserver/* /
 sh setup-server.sh
 rpm-ostree install --idempotent systemd-networkd wireless-regdb
 systemctl enable --now nftables systemd-networkd
+systemctl enable --global podman.socket
 systemctl disable --now NetworkManager
+systemd-tmpfiles --create
