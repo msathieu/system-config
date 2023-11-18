ac2f1470efbdf01d5ca10049196265008b19be50
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-18 09:32:44 +0100

    Add Gitea Actions

--- setup-server.sh
+++ setup-server.sh
@@ -3,4 +3,5 @@ set -e
 cp -R server/* /
 systemctl disable --now docker.socket rpm-ostree-countme.timer
 systemctl enable --now podman-auto-update.timer
+systemctl enable --global podman-auto-update.timer
 useradd -M containers || true
