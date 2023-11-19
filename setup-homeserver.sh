e34900691eaa98f2fa56ad22376a1e820a97d2a6
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-19 11:14:18 +0100

    Use podman-in-podman for act_runner

--- setup-homeserver.sh
+++ setup-homeserver.sh
@@ -4,6 +4,5 @@ cp -R homeserver/* /
 sh setup-server.sh
 rpm-ostree install --idempotent systemd-networkd wireless-regdb
 systemctl enable --now nftables systemd-networkd
-systemctl enable --global podman.socket
 systemctl disable --now NetworkManager
 systemd-tmpfiles --create
