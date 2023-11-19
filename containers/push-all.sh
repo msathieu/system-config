70a634f02c2660e61d516490bf6c4de24dec0ec3
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-19 16:46:44 +0100

    Switch to docker.io/alpine

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -2,13 +2,6 @@
 set -e
 cd "$(dirname "$0")"
 podman rmi -af
-alpine_url=https://dl-cdn.alpinelinux.org/alpine/v3.18/releases/x86_64/alpine-minirootfs-3.18.4-x86_64.tar.gz
-curl -o alpine.tar.gz $alpine_url
-curl -o alpine.tar.gz.asc $alpine_url.asc
-sq keyserver -s keyserver.ubuntu.com get 0482D84022F52DF1C4E7CD43293ACD0907D9495A > alpine.asc
-sq verify --signer-cert alpine.asc --detached alpine.tar.gz.asc alpine.tar.gz
-gunzip -f alpine.tar.gz
-mv alpine.tar alpine
 if ! podman login --get-login git.strypsteen.me; then
   podman login git.strypsteen.me --username mathieu
 fi
