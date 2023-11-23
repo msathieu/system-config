d0e54c015ffe0cd8992bfb651d41fbac66d9bfe6
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-11-23 21:19:04 +0100

    Switch gitea to git.strypsteen.com

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -2,11 +2,11 @@
 set -e
 cd "$(dirname "$0")"
 podman rmi -af
-if ! podman login --get-login git.strypsteen.me; then
-  podman login git.strypsteen.me --username mathieu
+if ! podman login --get-login git.strypsteen.com; then
+  podman login git.strypsteen.com --username mathieu
 fi
 for container in */; do
   container=${container%/}
   id=$(podman build --no-cache --squash -q "$container")
-  echo | podman push --sign-by-sigstore-private-key ~/Documents/cosign.key "$id" git.strypsteen.me/mathieu/"$container"
+  echo | podman push --sign-by-sigstore-private-key ~/Documents/cosign.key "$id" git.strypsteen.com/mathieu/"$container"
 done
