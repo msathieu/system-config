16788175a209bb649f0ffb1f1675bf068c488d6b
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-12-01 21:06:17 +0100

    Switch to upstream certbot container and run hostapd on host

--- setup-home.sh
+++ setup-home.sh
@@ -2,7 +2,7 @@
 set -e
 cp -R home/* /
 sh setup-server.sh
-rpm-ostree install --idempotent systemd-networkd wireless-regdb
+rpm-ostree install --idempotent hostapd systemd-networkd wireless-regdb
 systemctl enable --now nftables systemd-networkd
 systemctl disable --now NetworkManager
 systemd-tmpfiles --create
