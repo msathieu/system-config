23b2d587df2af8a18d96ce9756394850cd42a319
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-12-14 22:00:46 +0100

    Cleanup

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -6,4 +6,4 @@ systemctl enable --now sshd
 systemctl mask --global grub-boot-success.timer
 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
 flatpak remote-modify --subset=floss flathub
-flatpak install com.brave.Browser com.github.micahflee.torbrowser-launcher com.github.tchx84.Flatseal com.vscodium.codium com.yubico.yubioath im.riot.Riot io.mpv.Mpv org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.Snapshot org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox
+flatpak install com.github.Eloston.UngoogledChromium com.github.micahflee.torbrowser-launcher com.github.tchx84.Flatseal com.vscodium.codium com.yubico.yubioath im.riot.Riot io.mpv.Mpv org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.Snapshot org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox
