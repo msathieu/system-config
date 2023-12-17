982cb4f7ccf67784d50b77fdb9db52254b9d4ab2
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-12-17 17:55:26 +0100

    Finish router-home split

--- setup-home.sh
+++ setup-home.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 cp -R home/* /
+ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
 sh setup-server.sh
 systemd-tmpfiles --create
--- setup-server.sh
+++ setup-server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 cp -R server/* /
-systemctl disable --now docker.socket rpm-ostree-countme.timer
+systemctl disable --now auditd docker.socket rpm-ostree-countme.timer
 systemctl enable --now podman-auto-update.timer
 systemctl enable --global podman-auto-update.timer
 useradd -M containers || true
