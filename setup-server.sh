de4f11aeef862ef45773bdf2a0e9b4ebc76214b8
Mathieu Strypsteen <mathieu@strypsteen.me>
2023-12-23 18:59:41 +0100

    Minor changes

--- setup-server.sh
+++ setup-server.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 cp -R server/* /
-systemctl disable --now auditd docker.socket rpm-ostree-countme.timer
+systemctl disable auditd
+systemctl disable --now docker.socket rpm-ostree-countme.timer
 systemctl enable --now podman-auto-update.timer
 systemctl enable --global podman-auto-update.timer
 useradd -M containers || true
