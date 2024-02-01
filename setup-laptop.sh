6b2af4bc79dffb0db0200cda5a14a12bc528e20a
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-02-01 19:59:14 +0100

    Remove broken flathub filter

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -5,5 +5,4 @@ sh setup-desktop.sh
 systemctl enable --now sshd
 systemctl mask --global grub-boot-success.timer
 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
-flatpak remote-modify --subset=floss flathub
 flatpak install com.github.Eloston.UngoogledChromium com.github.tchx84.Flatseal com.vscodium.codium com.yubico.yubioath im.riot.Riot io.mpv.Mpv org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.Snapshot org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox org.torproject.torbrowser-launcher
