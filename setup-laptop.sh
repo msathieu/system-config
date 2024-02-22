78585026a9495c95ac3a1b99bc75b05d2191d852
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-02-22 21:21:01 +0100

    Add element-web

--- setup-laptop.sh
+++ setup-laptop.sh
@@ -5,4 +5,4 @@ sh setup-desktop.sh
 systemctl enable --now sshd
 systemctl mask --global grub-boot-success.timer
 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
-flatpak install com.github.tchx84.Flatseal com.github.xournalpp.xournalpp com.moonlight_stream.Moonlight com.vscodium.codium com.yubico.yubioath im.riot.Riot io.mpv.Mpv org.chromium.Chromium org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.Snapshot org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox org.torproject.torbrowser-launcher
+flatpak install com.github.tchx84.Flatseal com.github.xournalpp.xournalpp com.moonlight_stream.Moonlight com.yubico.yubioath io.mpv.Mpv org.chromium.Chromium org.gimp.GIMP org.gnome.Boxes org.gnome.Calculator org.gnome.Evince org.gnome.Evolution org.gnome.FileRoller org.gnome.Snapshot org.gnome.TextEditor org.libreoffice.LibreOffice org.mozilla.firefox org.torproject.torbrowser-launcher
