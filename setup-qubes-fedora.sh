9ca8409e6ba71a13e63bf9536769f32ed44508fc
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-03-21 21:27:48 +0100

    Use pipx

--- setup-qubes-fedora.sh
+++ setup-qubes-fedora.sh
@@ -2,7 +2,7 @@
 set -e
 cp -R desktop/* qubes-fedora/* /
 dnf config-manager --set-enabled qubes-vm-r4.2-security-testing qubes-vm-r4.2-current-testing rpmfusion-free rpmfusion-free-updates
-dnf install --allowerasing bash-completion bind-utils btrfs-progs butane default-fonts fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller flatseal fuse-sshfs gcc gcc-gdb-plugin gjs glibc-all-langpacks htop hunspell-* imv libreoffice mpv nautilus netcat qubes-ctap sequoia-sq ShellCheck toolbox vim-trailing-whitespace wireguard-tools whois xdg-desktop-portal-gtk yt-dlp
+dnf install --allowerasing bash-completion bind-utils butane default-fonts fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller flatseal fuse-sshfs gcc gcc-gdb-plugin gjs glibc-all-langpacks htop hunspell-* imv libreoffice mpv nautilus netcat nodejs-npm pipx qubes-ctap ShellCheck toolbox wireguard-tools whois xdg-desktop-portal-gtk yt-dlp
 dnf remove cheese gnome-software gnome-weather thunderbird totem
 sh setup-desktop.sh
 ln -sf /var/lib/flatpak/extension/org.mozilla.firefox.systemconfig/x86_64/stable/* /etc/firefox
