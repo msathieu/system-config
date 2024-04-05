4a39d48dc093ab3d48bc4cf943ed33f664ec00e8
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-04-05 21:31:38 +0200

    Switch to using Unifi APs

--- setup-router.sh
+++ setup-router.sh
@@ -2,7 +2,7 @@
 set -e
 cp -R router/* /
 sh setup-server.sh
-rpm-ostree install --idempotent hostapd systemd-networkd unbound wireless-regdb
+rpm-ostree install --idempotent systemd-networkd unbound
 systemctl enable --now nftables systemd-networkd unbound
 systemctl disable --now NetworkManager systemd-resolved
 systemctl mask unbound-anchor
