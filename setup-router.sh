e85c775dc85983b4cd46f5a939cdb7ea548a660f
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-04-09 19:14:41 +0200

    Remove router and add code-proxy

--- setup-router.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -e
-cp -R router/* /
-sh setup-server.sh
-rpm-ostree install --idempotent systemd-networkd
-systemctl enable --now nftables systemd-networkd
-systemctl disable --now NetworkManager systemd-resolved
