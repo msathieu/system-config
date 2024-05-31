73e113f29ac2035b7fae4d7b2c689ea7a7d9458d
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-05-31 09:53:09 +0200

    Add talos config

---
+++ setup-k8s.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+set -e
+helm upgrade --install metallb metallb --repo https://metallb.github.io/metallb --namespace metallb-system --create-namespace
+kubectl label namespaces metallb-system pod-security.kubernetes.io/enforce=privileged
+kubectl apply -f k8s/metallb.yaml
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
