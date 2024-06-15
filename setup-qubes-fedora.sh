f35e2da9179b078a40c1bcfc8b6ba5870d6c4011
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-06-15 20:29:18 +0200

    Also use flatpak on qubesos

--- setup-qubes-fedora.sh
+++ setup-qubes-fedora.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 cp -R desktop/* qubes-fedora/* /
-dnf config-manager --set-enabled qubes-vm-r4.2-security-testing qubes-vm-r4.2-current-testing rpmfusion-free rpmfusion-free-updates
-dnf install --allowerasing bash-color-prompt bash-completion bind-utils butane default-fonts fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller flatseal fuse-sshfs gcc gcc-gdb-plugin gjs glibc-all-langpacks helm htop hunspell-* imv kubernetes-client libreoffice mpv nautilus netcat nodejs-npm pipx qubes-ctap ShellCheck toolbox wireguard-tools whois xdg-desktop-portal-gtk yt-dlp
-dnf remove cheese evolution-data-server gnome-software gnome-weather PackageKit-command-not-found rpmfusion-nonfree-release thunderbird totem
+dnf config-manager --set-enabled qubes-vm-r4.2-security-testing qubes-vm-r4.2-current-testing
+dnf install --allowerasing bash-color-prompt bash-completion bind-utils butane default-fonts fcitx5-anthy fcitx5-autostart fedora-flathub-remote file-roller fuse-sshfs gcc gcc-gdb-plugin glibc-all-langpacks helm htop kubernetes-client nautilus netcat nodejs-npm pipx qubes-ctap ShellCheck toolbox wireguard-tools whois xdg-desktop-portal-gtk yt-dlp
+dnf remove cheese evolution-data-server firefox gnome-software gnome-weather PackageKit-command-not-found rpmfusion-nonfree-release thunderbird totem
 sh setup-desktop.sh
-ln -sf /var/lib/flatpak/extension/org.mozilla.firefox.systemconfig/x86_64/stable/* /etc/firefox
+all_proxy=127.0.0.1:8082 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
+all_proxy=127.0.0.1:8082 flatpak install com.github.tchx84.Flatseal io.mpv.Mpv org.chromium.Chromium org.libreoffice.LibreOffice org.mozilla.firefox
