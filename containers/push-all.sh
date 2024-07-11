28a3099c3cafbbae62c62d5e4d5f173bebda05ca
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-07-11 17:05:58 +0200

    Update code-server packages

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -e
 cd "$(dirname "$0")"
-podman rmi -af
 for container in */; do
   container=${container%/}
-  id=$(podman build --ulimit nofile=-1:-1 -q "$container")
+  id=$(podman build --pull=always --ulimit nofile=-1:-1 -q "$container")
   echo | podman push --compress --sign-by-sigstore-private-key ~/Documents/cosign.key "$id" git.strypsteen.com/mathieu/"$container"
 done
