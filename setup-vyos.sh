aa90d3370e9b0bd75930a408ddd050bd30c8206c
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-09-08 20:06:02 +0200

    Move monero node behind nginx

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -15,6 +15,7 @@ set system static-host-mapping host-name home.strypsteen.com alias network.stryp
 set system static-host-mapping host-name home.strypsteen.com alias office.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias vault.strypsteen.com
+set system static-host-mapping host-name home.strypsteen.com alias xmr.strypsteen.com
 set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
 
