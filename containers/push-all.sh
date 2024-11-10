23154667c7261c0ffe0b68397ce8d2d971d5f72a
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-11-10 23:01:31 +0100

    Move containers to git.strypsteen.com/infra/*

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")"
 for container in */; do
   container=${container%/}
   id=$(podman build --pull=always --ulimit nofile=-1:-1 -q "$container")
-  echo | podman push --compress --sign-by-sigstore-private-key ~/Documents/cosign.key "$id" git.strypsteen.com/mathieu/"$container"
+  echo | podman push --compress --sign-by-sigstore-private-key ~/Documents/cosign.key "$id" git.strypsteen.com/infra/"$container"
 done
