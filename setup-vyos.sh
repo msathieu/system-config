8ba0de2b3043f7de6de407e4ced36379feced293
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-12-09 13:36:22 +0100

    Fix vyos config

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -17,17 +17,17 @@ set system static-host-mapping host-name home.strypsteen.com alias photos.stryps
 set system static-host-mapping host-name home.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias vault.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias xmr.strypsteen.com
+set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
 
 set interfaces ethernet eth0 address dhcp
 set interfaces ethernet eth0 ipv6 address autoconf
-set interfaces ethernet eth0 dhcpv6-options pd 0 length 56
-set interfaces ethernet eth0 dhcpv6-options pd 0 interface eth1 sla-id 0
-set interfaces ethernet eth0 dhcpv6-options pd 0 interface eth2 sla-id 1
-set interfaces ethernet eth0 dhcpv6-options pd 0 interface eth3 sla-id 2
 set interfaces ethernet eth1 address 192.168.255.1/24
+set interfaces ethernet eth1 address fc00::1/64
 set interfaces ethernet eth2 address 192.168.254.1/24
+set interfaces ethernet eth2 address fc01::1/64
 set interfaces ethernet eth3 address 192.168.253.1/24
+set interfaces ethernet eth3 address fc02::1/64
 set interfaces wireguard wg0 address 192.168.252.1/24
 set interfaces wireguard wg0 port 51820
 
@@ -73,14 +73,13 @@ set service lldp interface all
 
 set nat source rule 1 outbound-interface name eth0
 set nat source rule 1 translation address masquerade
+set nat66 source rule 1 outbound-interface name eth0
+set nat66 source rule 1 translation address masquerade
 
 delete firewall
 
 set firewall global-options source-validation strict
 set firewall global-options ipv6-source-validation strict
-
-set firewall ipv6 forward filter rule 6 action accept
-set firewall ipv6 forward filter rule 6 protocol ipv6-icmp
 
 firewall_types="ipv4 ipv6"
 
