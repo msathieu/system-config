6bd1666a08706b382616bcb8f092e6990f38c1f3
Mathieu Strypsteen <mathieu@strypsteen.me>
2024-12-29 19:51:01 +0100

    Add second borg remote

---
+++ sync-changes.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+
+ssh infra "cd /var/lib/system-config; git pull; ./setup.sh infra"
+ssh home "cd /var/lib/system-config; git pull; ./setup.sh home"
+ssh sandbox "cd /var/lib/system-config; git pull; ./setup.sh sandbox"
+ssh vps "cd /var/lib/system-config; git pull; ./setup.sh vps"
