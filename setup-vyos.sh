b4b791e7dd9bb4cd70f8ad8bfe7859c3097c858f
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-05-13 15:37:48 +0200

    Use VLANs

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # shellcheck disable=SC2121
 set system name-server 9.9.9.9
-set system static-host-mapping host-name trace.svc.ui.com inet 0.0.0.0
+set system static-host-mapping host-name trace.svc.ui.com inet 0.0.0.1
 set system static-host-mapping host-name pve.strypsteen.com inet 192.168.253.2
 set system static-host-mapping host-name pbs.strypsteen.com inet 192.168.253.3
 set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.4
@@ -22,34 +22,51 @@ set system static-host-mapping host-name home.strypsteen.com alias photos.stryps
 set system static-host-mapping host-name home.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias textgen.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias vault.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias xmr.strypsteen.com
 set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
+set system update-check url https://raw.githubusercontent.com/vyos/vyos-nightly-build/refs/heads/current/version.json
 
 set interfaces ethernet eth0 address dhcp
 set interfaces ethernet eth0 ipv6 address autoconf
-set interfaces ethernet eth1 address 192.168.255.1/24
-set interfaces ethernet eth1 address fc00::1/64
-set interfaces ethernet eth2 address 192.168.254.1/24
-set interfaces ethernet eth2 address fc01::1/64
-set interfaces ethernet eth3 address 192.168.253.1/24
-set interfaces ethernet eth3 address fc02::1/64
+set interfaces bridge br0 stp
+set interfaces bridge br0 priority 0
+set interfaces bridge br0 enable-vlan
+set interfaces bridge br0 ip enable-proxy-arp
+set interfaces bridge br0 address 192.168.128.1/24
+set interfaces bridge br0 member interface eth1 allowed-vlan 1-4094
+set interfaces bridge br0 member interface eth1 native-vlan 1
+set interfaces bridge br0 member interface eth2 allowed-vlan 1-4094
+set interfaces bridge br0 member interface eth2 native-vlan 1
+set interfaces bridge br0 member interface eth3 allowed-vlan 1-4094
+set interfaces bridge br0 member interface eth3 native-vlan 253
+set interfaces bridge br0 vif 255 address 192.168.255.1/24
+set interfaces bridge br0 vif 255 address fc00::1/64
+set interfaces bridge br0 vif 254 address 192.168.254.1/24
+set interfaces bridge br0 vif 254 address fc01::1/64
+set interfaces bridge br0 vif 253 address 192.168.253.1/24
+set interfaces bridge br0 vif 253 address fc02::1/64
 set interfaces wireguard wg0 address 192.168.252.1/24
 set interfaces wireguard wg0 port 51820
 
 set service ssh disable-password-authentication
 set service dhcp-server hostfile-update
+set service dhcp-server shared-network-name infra option default-router 192.168.128.1
+set service dhcp-server shared-network-name infra option name-server 192.168.128.1
+set service dhcp-server shared-network-name infra subnet 192.168.128.0/24 subnet-id 128
+set service dhcp-server shared-network-name infra subnet 192.168.128.0/24 range 0 start 192.168.128.2
+set service dhcp-server shared-network-name infra subnet 192.168.128.0/24 range 0 stop 192.168.128.254
+set service dhcp-server shared-network-name infra subnet 192.168.128.0/24 lease 3600
 set service dhcp-server shared-network-name mgmt option default-router 192.168.255.1
 set service dhcp-server shared-network-name mgmt option domain-name mgmt.home.arpa
 set service dhcp-server shared-network-name mgmt option name-server 192.168.255.1
-set service dhcp-server shared-network-name mgmt subnet 192.168.255.0/24 subnet-id 1
+set service dhcp-server shared-network-name mgmt subnet 192.168.255.0/24 subnet-id 255
 set service dhcp-server shared-network-name mgmt subnet 192.168.255.0/24 range 0 start 192.168.255.2
 set service dhcp-server shared-network-name mgmt subnet 192.168.255.0/24 range 0 stop 192.168.255.254
 set service dhcp-server shared-network-name mgmt subnet 192.168.255.0/24 lease 3600
 set service dhcp-server shared-network-name lan option default-router 192.168.254.1
 set service dhcp-server shared-network-name lan option domain-name lan.home.arpa
 set service dhcp-server shared-network-name lan option name-server 192.168.254.1
-set service dhcp-server shared-network-name lan subnet 192.168.254.0/24 subnet-id 2
+set service dhcp-server shared-network-name lan subnet 192.168.254.0/24 subnet-id 254
 set service dhcp-server shared-network-name lan subnet 192.168.254.0/24 range 0 start 192.168.254.2
 set service dhcp-server shared-network-name lan subnet 192.168.254.0/24 range 0 stop 192.168.254.254
 set service dhcp-server shared-network-name lan subnet 192.168.254.0/24 lease 3600
@@ -58,16 +75,17 @@ set service dhcp-server shared-network-name server option bootfile-server 192.16
 set service dhcp-server shared-network-name server option default-router 192.168.253.1
 set service dhcp-server shared-network-name server option domain-name server.home.arpa
 set service dhcp-server shared-network-name server option name-server 192.168.253.1
-set service dhcp-server shared-network-name server subnet 192.168.253.0/24 subnet-id 3
+set service dhcp-server shared-network-name server subnet 192.168.253.0/24 subnet-id 253
 set service dhcp-server shared-network-name server subnet 192.168.253.0/24 range 0 start 192.168.253.5
 set service dhcp-server shared-network-name server subnet 192.168.253.0/24 range 0 stop 192.168.253.200
 set service dhcp-server shared-network-name server subnet 192.168.253.0/24 lease 3600
-set service router-advert interface eth1 prefix ::/64
-set service router-advert interface eth1 name-server fc00::1
-set service router-advert interface eth2 prefix ::/64
-set service router-advert interface eth2 name-server fc01::1
-set service router-advert interface eth3 prefix ::/64
-set service router-advert interface eth3 name-server fc02::1
+set service router-advert interface br0.255 prefix ::/64
+set service router-advert interface br0.255 name-server fc00::1
+set service router-advert interface br0.254 prefix ::/64
+set service router-advert interface br0.254 name-server fc01::1
+set service router-advert interface br0.253 prefix ::/64
+set service router-advert interface br0.253 name-server fc02::1
+set service dns forwarding listen-address 192.168.128.1
 set service dns forwarding listen-address 192.168.252.1
 set service dns forwarding listen-address 192.168.253.1
 set service dns forwarding listen-address 192.168.254.1
@@ -101,13 +119,24 @@ for i in ${firewall_types}; do
   set firewall "$i" forward filter rule 1 state established
   set firewall "$i" forward filter rule 1 state related
   set firewall "$i" forward filter rule 2 action accept
+  set firewall "$i" forward filter rule 2 inbound-interface name br0.254
   set firewall "$i" forward filter rule 2 outbound-interface name eth0
   set firewall "$i" forward filter rule 3 action accept
-  set firewall "$i" forward filter rule 3 inbound-interface name eth1
+  set firewall "$i" forward filter rule 3 inbound-interface name br0.253
+  set firewall "$i" forward filter rule 3 outbound-interface name eth0
   set firewall "$i" forward filter rule 4 action accept
-  set firewall "$i" forward filter rule 4 inbound-interface name eth2
-  set firewall "$i" forward filter rule 4 outbound-interface name eth3
+  set firewall "$i" forward filter rule 4 inbound-interface name br0.255
   set firewall "$i" forward filter rule 5 action accept
-  set firewall "$i" forward filter rule 5 inbound-interface name wg0
-  set firewall "$i" forward filter rule 5 outbound-interface name eth3
+  set firewall "$i" forward filter rule 5 inbound-interface name br0
+  set firewall "$i" forward filter rule 5 outbound-interface name br0.253
+  set firewall "$i" forward filter rule 6 action accept
+  set firewall "$i" forward filter rule 6 inbound-interface name wg0
+  set firewall "$i" forward filter rule 6 outbound-interface name br0.253
+  set firewall "$i" forward filter rule 7 action accept
+  set firewall "$i" forward filter rule 7 inbound-interface name br0.254
+  set firewall "$i" forward filter rule 7 outbound-interface name br0.253
 done
+set firewall ipv4 forward filter rule 8 action accept
+set firewall ipv4 forward filter rule 8 protocol tcp
+set firewall ipv4 forward filter rule 8 destination port 22
+set firewall ipv4 forward filter rule 8 inbound-interface name eth0
