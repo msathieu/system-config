58ee447bb1c9de3e0c4817fec9d7c2503e604a97
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-07-21 21:30:08 +0200

    Move big-agi to k8s

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -4,6 +4,8 @@ helm upgrade --install metallb metallb --repo https://metallb.github.io/metallb 
 kubectl label namespaces metallb-system pod-security.kubernetes.io/enforce=privileged
 kubectl apply -f k8s/metallb.yaml
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
-kubectl delete namespaces browserless || true
-kubectl create namespace browserless
-kubectl -n browserless apply -f k8s/browserless.yaml
+kubectl delete namespaces services || true
+kubectl create namespace services
+kubectl get secrets -o json | sed s/default/services/ | kubectl apply -f -
+kubectl -n services apply -f k8s/browserless.yaml
+kubectl -n services apply -f k8s/big-agi.yaml
