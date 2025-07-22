c7387dcd63732d14f3a3e19916b8e5d8a05b689b
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-07-22 07:54:32 +0200

    Add ingress and networkpolicy

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -4,8 +4,10 @@ helm upgrade --install metallb metallb --repo https://metallb.github.io/metallb 
 kubectl label namespaces metallb-system pod-security.kubernetes.io/enforce=privileged
 kubectl apply -f k8s/metallb.yaml
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
+helm upgrade --install cert-manager cert-manager --repo https://charts.jetstack.io --namespace cert-manager --create-namespace --set crds.enabled=true
 kubectl delete namespaces services || true
 kubectl create namespace services
 kubectl get secrets -o json | sed s/default/services/ | kubectl apply -f -
+kubectl -n services apply -f k8s/policy.yaml
 kubectl -n services apply -f k8s/browserless.yaml
 kubectl -n services apply -f k8s/big-agi.yaml
