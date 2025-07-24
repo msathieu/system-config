31c98a23a6f5cf7f09c8a7adee66952613cd5a20
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-07-24 14:46:41 +0200

    Add Longhorn

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 set -euo pipefail
-helm upgrade --install metallb metallb --repo https://metallb.github.io/metallb --namespace metallb-system --create-namespace
+kubectl create namespace longhorn-system || true
+kubectl -n longhorn-system apply -f k8s/longhorn-policy.yaml
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/refs/heads/master/examples/network-policy/webhook-network-policy.yaml
+kubectl label namespaces longhorn-system pod-security.kubernetes.io/enforce=privileged
+helm upgrade --install longhorn longhorn --repo https://charts.longhorn.io --namespace longhorn-system
+kubectl apply -f k8s/storageclass.yaml
+kubectl create namespace metallb-system || true
 kubectl label namespaces metallb-system pod-security.kubernetes.io/enforce=privileged
+helm upgrade --install metallb metallb --repo https://metallb.github.io/metallb --namespace metallb-system
 kubectl apply -f k8s/metallb.yaml
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace --set controller.extraArgs.default-ssl-certificate=services/k8s-certificate
 helm upgrade --install cert-manager cert-manager --repo https://charts.jetstack.io --namespace cert-manager --create-namespace --set crds.enabled=true --set 'extraArgs={--dns01-recursive-nameservers-only,--dns01-recursive-nameservers=1.1.1.1:53}'
