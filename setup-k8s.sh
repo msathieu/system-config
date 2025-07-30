ae4a3d1d72c71b645bdc09d3b8b0712ed82f2e07
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-07-30 11:11:45 +0200

    Move keycloak to k8s

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -4,7 +4,7 @@ kubectl create namespace longhorn-system || true
 kubectl -n longhorn-system apply -f k8s-infra/longhorn-policy.yaml
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/refs/heads/master/examples/network-policy/webhook-network-policy.yaml
 kubectl label namespaces longhorn-system pod-security.kubernetes.io/enforce=privileged
-helm upgrade --install longhorn longhorn --repo https://charts.longhorn.io --namespace longhorn-system
+helm upgrade --install longhorn longhorn --version 1.9.1 --repo https://charts.longhorn.io --namespace longhorn-system
 kubectl apply -f k8s-infra/storageclass.yaml
 kubectl create namespace metallb-system || true
 kubectl label namespaces metallb-system pod-security.kubernetes.io/enforce=privileged
@@ -15,6 +15,7 @@ helm upgrade --install cert-manager cert-manager --repo https://charts.jetstack.
 kubectl create namespace monitoring || true
 kubectl label namespaces monitoring pod-security.kubernetes.io/enforce=privileged
 helm upgrade --install prometheus kube-prometheus-stack --repo https://prometheus-community.github.io/helm-charts --namespace monitoring -f k8s-infra/monitoring-values.yaml
+helm upgrade --install cnpg cloudnative-pg --version 0.25.0 --repo https://cloudnative-pg.github.io/charts --namespace cnpg-system --create-namespace
 kubectl create namespace services || true
 kubectl get secrets -o json | sed s/default/services/ | kubectl apply -f - || true
 kubectl -n services apply -f k8s-apps/policy.yaml
@@ -24,3 +25,4 @@ kubectl -n services apply -f k8s-apps/big-agi.yaml
 kubectl -n services apply -f k8s-apps/unifi-mongodb.yaml
 kubectl -n services apply -f k8s-apps/unifi.yaml
 kubectl -n services apply -f k8s-apps/collabora.yaml
+kubectl -n services apply -f k8s-apps/keycloak.yaml
