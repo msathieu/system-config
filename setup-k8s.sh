c6bcf322f4caf533ed061b1ec240e9f3fe394e33
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-08-04 10:44:15 +0200

    Move more infra to argocd

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -6,9 +6,5 @@ kubectl create namespace longhorn-system || true
 kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/refs/tags/v1.9.1/examples/network-policy/webhook-network-policy.yaml
 kubectl label namespaces longhorn-system pod-security.kubernetes.io/enforce=privileged
 helm upgrade --install longhorn longhorn --version 1.9.1 --repo https://charts.longhorn.io --namespace longhorn-system -f k8s-infra/longhorn-values.yaml
-helm upgrade --install cert-manager cert-manager --repo https://charts.jetstack.io --namespace cert-manager --create-namespace -f k8s-infra/cert-manager-values.yaml
-kubectl create namespace monitoring || true
-kubectl label namespaces monitoring pod-security.kubernetes.io/enforce=privileged
-helm upgrade --install prometheus kube-prometheus-stack --repo https://prometheus-community.github.io/helm-charts --namespace monitoring -f k8s-infra/monitoring-values.yaml
 helm upgrade --install cnpg cloudnative-pg --version 0.25.0 --repo https://cloudnative-pg.github.io/charts --namespace cnpg-system --create-namespace
 kubectl create namespace services || true
