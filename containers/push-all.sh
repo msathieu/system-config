1721f3d3424d86da5abc060fbcbab29442c5c75d
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-08-16 23:15:36 +0200

    Further steps to move code-server to k8s

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 cd "$(dirname "$0")"
+cp ../common/etc/gitconfig code-server
 for container in */; do
   container=${container%/}
   id=$(podman build --pull=always --ulimit nofile=-1:-1 -q "$container")
