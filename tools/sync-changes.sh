ea2c86b56247a1e992b087fc5fabaa10e49ee67f
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-08-19 18:14:29 +0200

    k8s updates

--- tools/sync-changes.sh
+++ tools/sync-changes.sh
@@ -2,6 +2,5 @@
 set -euo pipefail
 
 ssh infra "cd /var/lib/system-config; git pull; ./setup.sh infra"
-ssh sandbox "cd /var/lib/system-config; git pull; ./setup.sh sandbox"
 ssh vps "cd /var/lib/system-config; git pull; ./setup.sh vps"
 ssh gpu "cd /var/lib/system-config; git pull; ./setup.sh gpu"
