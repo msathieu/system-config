9b7d408713babfb7a1524d35cafdcec80e1fc827
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-09-19 08:36:01 +0200

    k8s updates

--- setup-qubes.sh
+++ setup-qubes.sh
@@ -4,7 +4,7 @@ sudo cp -R qubes/* /
 qvm-pool set varlibqubes -o ephemeral_volatile=True
 qvm-features dom0 gui-default-secure-copy-sequence Ctrl-Mod4-c
 qvm-features dom0 gui-default-secure-paste-sequence Ctrl-Mod4-v
-sudo qubes-dom0-update kde-settings-qubes qubes-ctap-dom0 qubes-screenshot-helper
+sudo qubes-dom0-update kde-settings-qubes qubes-ctap-dom0 qubes-desktop-linux-common-wayland qubes-screenshot-helper
 qvm-features sys-net ipv6 1
 qvm-features sys-whonix ipv6 ""
 qvm-prefs fedora-42 kernelopts "swiotlb=2048 lockdown=confidentiality"
