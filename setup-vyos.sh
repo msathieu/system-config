4ba0ddaf0fd0eef8832fbc26621afd9f83f89f31
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-09-27 13:10:07 +0200

    Use longhorn-custom storage class for forgejo-runner

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -2,7 +2,6 @@
 # shellcheck disable=SC2121
 set system name-server 9.9.9.9
 set system static-host-mapping host-name trace.svc.ui.com inet 0.0.0.1
-set system static-host-mapping host-name pve.strypsteen.com inet 192.168.253.2
 set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.31
 set system static-host-mapping host-name k8s.strypsteen.com inet 192.168.253.30
 set system static-host-mapping host-name k8s.strypsteen.com alias unifi
