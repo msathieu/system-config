997732e99d688b989953f074db48ff2e1076a965
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-10-01 14:15:26 +0200

    Switch to fedora container images

--- setup-qubes-fedora.sh
+++ setup-qubes-fedora.sh
@@ -5,7 +5,7 @@ dnf config-manager setopt qubes-vm-r4.3-security-testing.enabled=true
 dnf config-manager setopt qubes-vm-r4.3-current-testing.enabled=true
 dnf config-manager setopt rpmfusion-free.enabled=true
 dnf config-manager setopt rpmfusion-free-updates.enabled=true
-dnf install --allowerasing bash-color-prompt bash-completion borgbackup bind-utils butane default-fonts fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller fuse-sshfs gcc gcc-gdb-plugin glibc-all-langpacks helm htop kubernetes1.33-client nautilus netcat nodejs-npm pipx qubes-ctap ShellCheck tigervnc toolbox wireguard-tools whois xdg-desktop-portal-gtk
+dnf install --allowerasing bash-color-prompt bash-completion borgbackup bind-utils butane default-fonts fcitx5-anthy fcitx5-autostart fedora-flathub-remote ffmpeg file-roller fuse-sshfs gcc gcc-gdb-plugin glibc-all-langpacks helm htop kubernetes1.34-client nautilus netcat nodejs-npm pipx qubes-ctap ShellCheck tigervnc toolbox wireguard-tools whois xdg-desktop-portal-gtk
 dnf remove cheese evolution-data-server firefox gnome-software gnome-weather PackageKit-command-not-found rpmfusion-nonfree-release thunderbird totem
 bash setup-desktop.sh
 sudo systemctl enable --global ssh-agent.socket
