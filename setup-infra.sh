f3b9c4535812b5e662f75de69961dfd035ff54b8
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-01 20:27:24 +0100

    Move invokeai to k8s ingress

--- setup-infra.sh
+++ setup-infra.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 cp -R infra/* /
 bash setup-server.sh
-systemctl enable --now certbot.timer atlas-socat forgejo-socat wg-quick@gpu wg-quick@home-gw
+systemctl enable --now certbot.timer atlas-socat forgejo-socat wg-quick@home-gw
