3c97d664b7f7e369fa3631e616082ff96e8911fd
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-17 22:42:11 +0100

    Use actual hostnames in k8s

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -2,9 +2,8 @@
 # shellcheck disable=SC2121
 set system name-server 9.9.9.9
 set system static-host-mapping host-name trace.svc.ui.com inet 0.0.0.1
-set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.31
-set system static-host-mapping host-name k8s.strypsteen.com inet 192.168.253.30
-set system static-host-mapping host-name k8s.strypsteen.com alias unifi
+set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.30
+set system static-host-mapping host-name home.strypsteen.com alias unifi
 set system static-host-mapping host-name home.strypsteen.com alias auth.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias backups.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias cloud.strypsteen.com
