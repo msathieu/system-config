6c13b2140824f366f3309d21693fa0d94838c1e7
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-19 23:00:10 +0100

    Add netbird to home-gw

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -5,5 +5,5 @@ cp ../common/etc/gitconfig code-server
 for container in */; do
   container=${container%/}
   id=$(podman build --pull=always --ulimit nofile=-1:-1 -q "$container")
-  echo | podman push --compress --sign-by-sigstore-private-key ~/Documents/cosign.key "$id" git.strypsteen.com/infra/"$container"
+  podman push --compress "$id" git.strypsteen.com/infra/"$container"
 done
