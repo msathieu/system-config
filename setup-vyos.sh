45ece162a6dc8c10081cb83d8a75d81ea69ac266
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-23 14:34:25 +0100

    Move atlas, nextcloud, vaultwarden and keycloak to envoy

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -4,17 +4,19 @@ set system name-server 9.9.9.9
 set system static-host-mapping host-name trace.svc.ui.com inet 0.0.0.1
 set system static-host-mapping host-name k8s.strypsteen.com inet 192.168.253.30
 set system static-host-mapping host-name k8s.strypsteen.com alias unifi
+set system static-host-mapping host-name k8s-gw.strypsteen.com inet 192.168.253.34
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias auth.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias cloud.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias console.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias git.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias matrix.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias vault.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.31
-set system static-host-mapping host-name home.strypsteen.com alias auth.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias backups.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias cloud.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias code.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias code-sandbox.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias console.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias git.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias imagegen.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias llm.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias matrix.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias metrics.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias monitoring.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias mta-sts.strypsteen.com
@@ -23,7 +25,6 @@ set system static-host-mapping host-name home.strypsteen.com alias office.stryps
 set system static-host-mapping host-name home.strypsteen.com alias photos.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias textgen.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias vault.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias mta-sts.strypsteen.me
 set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
