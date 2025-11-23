a3cd254681d93a30977ab8fe8166642ef054d302
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-23 21:46:06 +0100

    Move grafana and caddy-static to envoy

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -9,12 +9,15 @@ set system static-host-mapping host-name k8s-gw.strypsteen.com alias cloud.stryp
 set system static-host-mapping host-name k8s-gw.strypsteen.com alias console.strypsteen.com
 set system static-host-mapping host-name k8s-gw.strypsteen.com alias git.strypsteen.com
 set system static-host-mapping host-name k8s-gw.strypsteen.com alias matrix.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias mta-sts.strypsteen.com
 set system static-host-mapping host-name k8s-gw.strypsteen.com alias vault.strypsteen.com
+set system static-host-mapping host-name k8s-gw.strypsteen.com alias mta-sts.strypsteen.me
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com inet 192.168.253.35
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias unifi
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias backups.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias code.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias llm.strypsteen.com
+set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias monitoring.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias network.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias office.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias photos.strypsteen.com
@@ -22,11 +25,8 @@ set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.31
 set system static-host-mapping host-name home.strypsteen.com alias code-sandbox.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias imagegen.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias metrics.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias monitoring.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias mta-sts.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias textgen.strypsteen.com
-set system static-host-mapping host-name home.strypsteen.com alias mta-sts.strypsteen.me
 set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
 set system update-check url https://raw.githubusercontent.com/vyos/vyos-nightly-build/refs/heads/current/version.json
