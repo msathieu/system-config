95f5d15bd4a170746e3f7ff85d649a77df9148e9
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-24 22:12:42 +0100

    Move invokeai to envoy

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -17,6 +17,7 @@ set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias un
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias backups.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias code.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias code-sandbox.strypsteen.com
+set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias imagegen.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias llm.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias monitoring.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias network.strypsteen.com
@@ -25,7 +26,6 @@ set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias ph
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias textgen.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com inet 192.168.253.31
-set system static-host-mapping host-name home.strypsteen.com alias imagegen.strypsteen.com
 set system static-host-mapping host-name home.strypsteen.com alias metrics.strypsteen.com
 set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
