8d8b1538bdf1526a4da19f89012ec5d5f22766a8
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-11-27 09:25:23 +0100

    Enable ssh-agent in code-server

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 cd "$(dirname "$0")"
 cp ../common/etc/gitconfig code-server
+cp ../common/etc/ssh/ssh_config code-server
 for container in */; do
   container=${container%/}
   id=$(podman build --pull=always --ulimit nofile=-1:-1 -q "$container")
