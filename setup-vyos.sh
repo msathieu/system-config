24b84ac9b98547ae2821e022aca8d3fbdea77df2
Mathieu Strypsteen <mathieu@strypsteen.me>
2025-12-07 19:53:08 +0100

    Add rook operator

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -26,7 +26,6 @@ set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias ph
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias remote-desktop.strypsteen.com
 set system static-host-mapping host-name k8s-gw-internal.strypsteen.com alias textgen.strypsteen.com
 set system static-host-mapping host-name k8s-test.strypsteen.com inet 192.168.253.36
-set system static-host-mapping host-name cdi.strypsteen.com inet 192.168.253.37
 set system sysctl parameter net.ipv6.conf.eth0.use_tempaddr value 2
 set system time-zone Europe/Brussels
 set system update-check url https://raw.githubusercontent.com/vyos/vyos-nightly-build/refs/heads/current/version.json
