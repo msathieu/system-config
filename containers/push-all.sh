bb452d6afd2d4702fbe496cf182796e51d066ede
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-03-11 09:55:04 +0100

    k8s updates

--- containers/push-all.sh
+++ containers/push-all.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 cd "$(dirname "$0")"
 cp ../common/etc/gitconfig code-server
 cp ../common/etc/ssh/ssh_config code-server
+cp ../common/etc/vimrc sandboxed-toolbox
 for container in */; do
   container=${container%/}
   id=$(podman build --pull=always --ulimit nofile=-1:-1 -q "$container")
