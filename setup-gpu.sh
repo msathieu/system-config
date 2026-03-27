d7a550c1868983ec1b36ed06799e81207663facf
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-03-27 09:44:17 +0100

    k8s updates

--- setup-gpu.sh
+++ setup-gpu.sh
@@ -6,4 +6,4 @@ bash setup-ostree.sh
 rpm-ostree install --idempotent akmod-nvidia cage libvirt libvirt-daemon-kvm nvidia-container-toolkit ramalama steam-devices waydroid xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
 systemctl enable --now libvirtd.service podman-auto-update.timer sshd sunshine
 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
-flatpak install com.github.tchx84.Flatseal com.usebottles.bottles com.valvesoftware.Steam com.valvesoftware.Steam.CompatibilityTool.Proton-GE dev.lizardbyte.app.Sunshine org.chromium.Chromium org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 org.gnome.Calculator org.gnome.FileRoller org.gnome.TextEditor org.mozilla.firefox
+flatpak install com.github.tchx84.Flatseal com.usebottles.bottles com.valvesoftware.Steam com.vysp3r.ProtonPlus dev.lizardbyte.app.Sunshine org.chromium.Chromium org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 org.gnome.Calculator org.gnome.TextEditor org.mozilla.firefox
