423b4ed8858684d67b9b72efbf96ca165f91bf8b
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-04-17 17:07:10 +0200

    k8s updates

--- setup-k8s.sh
+++ setup-k8s.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
-talosctl gen config sakura https://k8s-controlplane.strypsteen.com:6443 --config-patch @"$(dirname "$0")"/talos/talos.yaml --with-secrets secrets.yaml --kubernetes-version 1.35.3 --force
+talosctl gen config sakura https://k8s-controlplane.strypsteen.com:6443 --config-patch @"$(dirname "$0")"/talos/talos.yaml --with-secrets secrets.yaml --kubernetes-version 1.35.4 --force
 talosctl apply-config -n k8s-1 -f controlplane.yaml -p @"$(dirname "$0")"/talos/talos-1.yaml
 talosctl apply-config -n k8s-2 -f controlplane.yaml -p @"$(dirname "$0")"/talos/talos-2.yaml
 talosctl apply-config -n k8s-3 -f controlplane.yaml -p @"$(dirname "$0")"/talos/talos-3.yaml
