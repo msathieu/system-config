5ac19385fd67672967c1be196f4966e4711dcdb5
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-04-30 00:00:07 +0200

    k8s updates

--- setup-qubes-fedora.sh
+++ setup-qubes-fedora.sh
@@ -11,4 +11,4 @@ bash setup-desktop.sh
 sudo systemctl enable nix-daemon
 sudo systemctl enable --global ssh-agent.socket
 all_proxy=127.0.0.1:8082 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
-all_proxy=127.0.0.1:8082 flatpak install com.github.johnfactotum.Foliate com.github.tchx84.Flatseal com.github.xournalpp.xournalpp com.moonlight_stream.Moonlight com.yubico.yubioath im.riot.Riot io.mpv.Mpv net.cozic.joplin_desktop net.mullvad.MullvadBrowser org.chromium.Chromium org.gimp.GIMP org.gnome.Evolution org.libreoffice.LibreOffice org.mozilla.firefox org.virt_manager.virt-manager
+all_proxy=127.0.0.1:8082 flatpak install com.github.johnfactotum.Foliate com.github.tchx84.Flatseal com.github.xournalpp.xournalpp com.moonlight_stream.Moonlight com.yubico.yubioath im.riot.Riot io.mpv.Mpv net.cozic.joplin_desktop net.mullvad.MullvadBrowser org.chromium.Chromium org.gimp.GIMP org.gnome.Evolution org.libreoffice.LibreOffice org.mozilla.firefox org.signal.Signal org.virt_manager.virt-manager
