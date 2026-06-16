73d35b7efe31a61dc10a30a26eff0323c6496ad8
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-06-16 08:32:09 +0200

    k8s updates

--- setup-gpu.sh
+++ setup-gpu.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 cp -R desktop/* gpu/* /
 bash setup-desktop.sh
 bash setup-ostree.sh
-rpm-ostree install --idempotent akmod-nvidia cage libvirt libvirt-daemon-kvm nvidia-container-toolkit waydroid xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
+rpm-ostree install --idempotent akmod-nvidia libvirt libvirt-daemon-kvm nvidia-container-toolkit xorg-x11-drv-nvidia xorg-x11-drv-nvidia-cuda
 systemctl enable --now libvirtd.service podman-auto-update.timer sshd sunshine
 flatpak remote-add --if-not-exists flathub /usr/lib/fedora-third-party/conf.d/fedora-flathub.flatpakrepo
 flatpak install com.brave.Browser com.github.mtkennerly.ludusavi com.github.tchx84.Flatseal com.usebottles.bottles com.valvesoftware.Steam com.vysp3r.ProtonPlus dev.lizardbyte.app.Sunshine org.freedesktop.Platform.VulkanLayer.MangoHud/x86_64/25.08 org.gnome.Calculator org.gnome.TextEditor org.mozilla.firefox org.prismlauncher.PrismLauncher
