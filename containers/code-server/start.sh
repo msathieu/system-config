63a0d7300fea350620053b175fa783d5ab99da6e
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-07-12 17:00:35 +0200

    Change code-server port

--- containers/code-server/start.sh
+++ containers/code-server/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 nix-daemon &
-su coder -c "ssh-agent code-server --bind-addr 0.0.0.0:8080 --disable-telemetry --proxy-domain $PROXY_DOMAIN"
+su coder -c "ssh-agent code-server --bind-addr 0.0.0.0:9000 --disable-telemetry --proxy-domain $PROXY_DOMAIN"
