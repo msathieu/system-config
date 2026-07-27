7adfc4e53037ec90e59e814b46d270c8b68e1967
Mathieu Strypsteen <mathieu@strypsteen.me>
2026-07-27 18:47:47 +0200

    Add *.devproxy.console.strypsteen.com

--- setup-vyos.sh
+++ setup-vyos.sh
@@ -99,6 +99,7 @@ set service dns forwarding allow-from 192.168.0.0/16
 set service dns forwarding allow-from 100.81.0.0/16
 set service dns forwarding allow-from fc00::/7
 set service dns forwarding authoritative-domain dev.console.strypsteen.com records a any address 192.168.253.34
+set service dns forwarding authoritative-domain devproxy.console.strypsteen.com records a any address 192.168.253.34
 set service dns forwarding authoritative-domain code.strypsteen.com records a any address 192.168.253.35
 set service dns forwarding authoritative-domain code.strypsteen.com records a @ address 192.168.253.35
 set service dns forwarding authoritative-domain code-sandbox.strypsteen.com records a any address 192.168.253.35
